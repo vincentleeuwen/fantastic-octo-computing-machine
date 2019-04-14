2eff3d4792d502f5e0cff14e44f5ea87764c6b17
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2019-04-14 16:11:42 +0200

    corrected winner function (typo 0 v.s. o

--- src/Board.js
+++ src/Board.js
@@ -11,49 +11,50 @@ class Board extends React.Component {
     };
   }
 
-handleClick(i) {
-  const squares = this.state.squares.slice();
-  squares[i] = this.state.xIsNext ? "x" : "O";
-  this.setState({
-    squares: squares,
-    xIsNext: !this.state.xIsNext,
-  });
-}
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? "x" : "o";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
 
   renderSquare(i) {
     return (
-    <Square
-    value={this.state.squares[i]}
-    onClick={()=> this.handleClick(i)}
-     />
-     );
+      <Square
+      value={this.state.squares[i]}
+      onClick={()=> this.handleClick(i)}
+      />
+      );
   }
 
- render() {
+  render() {
 
     const winner = calculateWinner(this.state.squares);
+    console.log(winner);
     const status = getStatus(winner, this.state);
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
       </div>
-    );
+      <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+      </div>
+      </div>
+      );
   }
 }
 
@@ -61,14 +62,14 @@ export default Board
 
 function calculateWinner(squares) {
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
@@ -87,9 +88,9 @@ function getPlayersName(player) {
 }
 
 function getStatus(winner, state) {
-    if (winner)
-      return 'Winner: ' + getPlayersName(winner);
-    else
-      return 'Jetzt spielt:' + getPlayersName(state.xIsNext ? 'x' : 'o');
+  if (winner)
+    return ('Winner: ' + getPlayersName(winner));
+  else
+    return 'Jetzt spielt:' + getPlayersName(state.xIsNext ? 'x' : 'o');
 }
 
